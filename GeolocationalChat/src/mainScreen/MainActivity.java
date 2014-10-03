f283baf3276f82457ba66a999769456c65082ae3
Josh Heinrichs (jih320) <josh.heinrichs@usask.ca>
2014-10-03 16:05:32 -0600

    Added basic listing to main page

--- GeolocationalChat/src/mainScreen/MainActivity.java
+++ GeolocationalChat/src/mainScreen/MainActivity.java
@@ -5,16 +5,33 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    	super.onCreate(savedInstanceState);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+            "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+            "Linux", "OS/2", "Foo", "Bar", "More", "Does this scroll?" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+            android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
     }
-
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+      String item = (String) getListAdapter().getItem(position);
+      Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
