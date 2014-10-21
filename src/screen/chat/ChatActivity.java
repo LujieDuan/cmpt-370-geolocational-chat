631763ee15a0e42bf860035374d212e341c053b5
66813185 <Doris@MacBook-Pro.local>
2014-10-20 22:15:07 -0600

    Test connection with database through PHP
    1.php files on free host: cmpt370duan.byethost10.com
    2.build temp table in university mySQL
    3.connect app with php and fetch data from temp table
    4.still need: more tables, more php files for more functions,
    JSONParser in app for formatting.

--- src/screen/chat/ChatActivity.java
+++ src/screen/chat/ChatActivity.java
@@ -1,10 +1,22 @@
 package screen.chat;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,13 +29,65 @@ import coderunners.geolocationalchat.R;
 
 public class ChatActivity extends Activity
 {
+//  JSONParser jParser = new JSONParser();
+//  ArrayList<HashMap<String, String>> productsList = new ArrayList<HashMap<String, String>>();
+//  
+//  // url to get all products list
+//  private static String url_all_products = "http://192.168.0.14/android/getSomeThing.php";
+//
+//  // JSON Node names
+//  private static final String TAG_SUCCESS = "success";
+//  private static final String TAG_PRODUCTS = "products";
+//  private static final String TAG_PID = "pid";
+//  private static final String TAG_NAME = "name";
+//
+//  // products JSONArray
+//  JSONArray products = null;
+
+	
 	ArrayList<ChatItem> valueList = new ArrayList<ChatItem>();	  
 	  
 	MySimpleArrayAdapter adapter;
 	  
-	@Override
+	@SuppressLint("NewApi") @Override
 	  protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		
+		//for Internet!!!!
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy); 
+		
+		
+		  String result = "";
+		    InputStream isr = null;
+		    try{
+		    	HttpClient httpclient = new DefaultHttpClient();
+		    	HttpPost httppost = new HttpPost("http://cmpt370duan.byethost10.com/getSomeThing.php");
+		    	HttpResponse response = httpclient.execute(httppost);
+		    	HttpEntity entity = response.getEntity();
+		    	isr = entity.getContent();
+		    }catch(Exception e){
+		    	Log.e("log_tag", "Error in http connection " + e.toString());
+		    }
+
+		    try{
+		    	BufferedReader reader = new BufferedReader(new InputStreamReader(isr, "iso-8859-1"), 8);
+		    	StringBuilder sb = new StringBuilder();
+		    	String line = null;
+		    	while ((line = reader.readLine()) != null){
+		    			sb.append(line+ "..");
+		    			
+		    	}
+		    	isr.close();
+		    	result = sb.toString();
+		    }
+		    catch (Exception e){
+		    	Log.e("log_tag", "Error converting result" + e.toString());
+		    }
+		    
+		    Log.e("log_tagggggg", "!!!!" + result + "ddd");
+		
 		
 		valueList.add(new ChatItem("Mike", "Programming Contest on weekend!", "3 hours ago", "200m") );
 		valueList.add(new ChatItem("Tom","I will be there!", "2 hours ago", "10m") );
@@ -31,7 +95,7 @@ public class ChatActivity extends Activity
 		valueList.add(new ChatItem("Will", "Nice:-", "2 hours ago", "50m") );
 		valueList.add(new ChatItem("Anthony", "How much is the ticket?", "1 hour ago", "400m") );
 		valueList.add(new ChatItem("Me", "On what platform?", "1 hour ago", "0m") );
-		valueList.add(new ChatItem("William van der Kamp", "Windows I think", " just now", "200m") );
+		valueList.add(new ChatItem("William van der Kamp", result, " just now", "200m") );//for database debug!!!!
 		
 	    setContentView(R.layout.chat_screen);
 
